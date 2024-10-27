372922e47e3128e5e218fe48f1781f1a810a2dd3
divya.sharma <ddivyasharmaa4@gmail.com>
2024-10-27 15:26:45 +0530

    add success alert on email submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 (function ($) {
     "use strict";
 
+    if (window.location.href.indexOf('subscribed') !== -1) {
+        $('#alert-subscribed').prop('hidden', false);
+    }
+
     const remainingSec = $('.countdown').data('remaining-sec');
     $('.countdown').ClassyCountdown({
         theme: "flat-colors-very-wide",
